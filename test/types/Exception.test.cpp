3ccb5e4d95a5c97a2145d805fa7138ef00c5df9a
woutervanbakel <wouter.vb@gmail.com>
2020-12-18 15:18:10 +0100

    Measure code coverage

--- test/types/Exception.test.cpp
+++ test/types/Exception.test.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "gtest/gtest.h"
+#include "types/Exception.hpp"
 
 TEST(Excpetion, initialTest) {
 	EXPECT_EQ(0, 0);
+}
+
+TEST(ExceptionTest, ShouldReturnErrAsJson) {
+    Exception ex("Exception desc");
+    EXPECT_STREQ(ex.what(), "Exception desc");
 }
